da2b9654069eeea2366b5f8c6aa9ff7f57d04507
anacarde <lazare.monin@gmail.com>
2019-06-07 18:34:00 +0200

    ajout d'autres méthodes des routes dans la classe correspondante

--- src/controller/VisitorPageController.php
+++ src/controller/VisitorPageController.php
@@ -11,4 +11,29 @@ class VisitorPageController extends Controller
         echo $this->view("visHomeBlock.html.twig", [
         ]);
     }
+
+    public function goToMenu() {
+        echo $this->view("visMenuBlock.html.twig", [
+        ]);
+    }
+
+    public function goToModel() {
+        echo $this->view("visModelBlock.html.twig", [
+        ]);
+    }
+
+    public function goToGameOne() {
+        echo $this->view("visGameOneBlock.html.twig", [
+        ]);
+    }
+
+    public function goToGameTwo() {
+        echo $this->view("visGameTwoBlock.html.twig", [
+        ]);
+    }
+
+    public function goToResult() {
+        echo $this->view("visResultBlock.html.twig", [
+        ]);
+    }
 }
