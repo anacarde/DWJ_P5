84f40181b59a335cfa5c1a0931d88880458d6841
anacarde <lazare.monin@gmail.com>
2019-06-09 10:07:35 +0200

    ajout des événements de changement de la direction selon le choix du niveau

--- public/js/menuJs.js
+++ public/js/menuJs.js
@@ -1,10 +1,42 @@
-var colFamOpt = document.getElementById("col-fam-opt");
-var colRandOpt = document.getElementById("col-rand-opt");
-var famLs = document.getElementById("fam-ls");
+var Sel = {
+    form: document.getElementById("sel-form"),
+    colFamOpt: document.getElementById("col-fam-opt"),
+    colRandOpt: document.getElementById("col-rand-opt"),
+    famLs: document.getElementById("fam-ls"),
+    appLev: document.getElementById("app-lev-lab"),
+    expLev: document.getElementById("exp-lev-lab"),
+}
 
-colFamOpt.addEventListener('click', function() {
-    famLs.classList.remove('invisible');
-})
-colRandOpt.addEventListener('click', function() {
-    famLs.classList.add('invisible');
-})
+function MenuManager() {
+
+    this.managerEvts = function() {
+        
+        Sel.colFamOpt.addEventListener('click', function() {
+            Sel.famLs.classList.remove('invisible');
+        })
+
+        Sel.colRandOpt.addEventListener('click', function() {
+            Sel.famLs.classList.add('invisible');
+        })
+
+        Sel.appLev.addEventListener('click', function() {
+            if(Sel.form.getAttribute("action") !== "/play/model") {
+                Sel.form.setAttribute("action", "/play/model");
+            }
+        })
+
+        Sel.expLev.addEventListener('click', function() {
+            if(Sel.form.getAttribute("action") !== "/play/game-one") {
+                Sel.form.setAttribute("action", "/play/game-one");
+            }
+        })
+    }
+
+}
+
+var menuManager = new MenuManager;
+
+menuManager.managerEvts();
+
+
+
