fc85354c441c82300b7c9d7c31a3db725e54f4cd
anacarde <lazare.monin@gmail.com>
2019-06-16 19:47:33 +0200

    ajout du retrait du script lors de l'appui sur la croix

--- public/js/addColJs.js
+++ public/js/addColJs.js
@@ -1,13 +1,16 @@
 var Sel = {
+    body: document.querySelector("body"),
     closeCross: document.getElementById("close-cross"),
     contBlo: document.getElementById("cont-blo"),
     addColDiv: document.getElementById("add-col-div"),
+    addColScr: document.getElementById("add-col-scr"),
 }
 
 function AddColManager() {
 
     this.closeCrossFn = function() {
         Sel.contBlo.removeChild(Sel.addColDiv);
+        Sel.body.removeChild(Sel.addColScr);
     }
 
     this.closeCrossEvt = function() {
