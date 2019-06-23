1c94c382624e95281dca204c86b518ad7fca6475
anacarde <lazare.monin@gmail.com>
2019-06-23 18:15:33 +0200

    ajout des fichiers .env et .env.local pour les informations sensibles

--- public/index.php
+++ public/index.php
@@ -4,6 +4,16 @@ require_once __DIR__."/../vendor/autoload.php";
 
 use App\Router\Router;
 use Zend\Diactoros\ServerRequestFactory;
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/../.env', __DIR__.'/../.env.local');
+
+/*var_dump($_ENV['DB_USER']);
+var_dump($_ENV['DB_PASS']);
+return;
+*/
+
 
 $request = ServerRequestFactory::fromGlobals();
 
